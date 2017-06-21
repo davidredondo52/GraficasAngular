9667f54507a9f65adfdcd1068d3aa96b8292e02d
davidredondo52 <davidredondo52@hotmail.com>
2017-06-21 13:39:49 +0200

    Fin

---
+++ src/app/components/donut/donut.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-donut',
+  templateUrl: './donut.component.html',
+  styleUrls: ['./donut.component.css']
+})
+export class DonutComponent  {
+
+  // Doughnut
+  public doughnutChartLabels:string[] = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales'];
+  public doughnutChartData:number[] = [350, 450, 100];
+  public doughnutChartType:string = 'doughnut';
+ 
+  // events
+  public chartClicked(e:any):void {
+    console.log(e);
+  }
+ 
+  public chartHovered(e:any):void {
+    console.log(e);
+  }
+
+  public numeros_random()
+  {
+  	this.doughnutChartData=[
+  	Math.round(Math.random()*100),
+  	Math.round(Math.random()*100),
+  	Math.round(Math.random()*100)
+  	]
+  }
+
+}
